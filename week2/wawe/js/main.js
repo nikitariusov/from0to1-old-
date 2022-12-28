f939ec09d6902c106d6b75d033a97c4dfbd5e45d
Nikita Usov <nikitariusov@gmail.com>
2022-12-29 00:37:58 +0200

    wawe: add menu button to adaptive

--- week2/wawe/js/main.js
+++ week2/wawe/js/main.js
@@ -1,5 +1,10 @@
 console.log('js is connect');
 $(function() {
+
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').toggleClass('menu__list--active')
+  })
+
   let mixer = mixitup('.gallery__content', {
     animation: {
       enable: false
